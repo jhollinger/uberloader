198d198495a59b30d1a3a6204f63e57b5afa8a21
Jordan Hollinger <jordan.hollinger@gmail.com>
2024-06-21 10:02:37 -0400

    find_each and find_in_batches

--- lib/uberloader/query.rb
+++ lib/uberloader/query.rb
@@ -16,17 +16,38 @@ module Uberloader
       records
     end
 
-    #def find_each
-    #end
-
-    #def find_in_batches
-    #end
-
     def each
       if block_given?
         to_a.each { |row| yield row }
       else
         to_a.each
+      end
+    end
+
+    def find_each(start: nil, finish: nil, batch_size: 1000, error_on_ignore: nil)
+      enum = Enumerator.new { |y|
+        find_in_batches(start: start, finish: finish, batch_size: batch_size, error_on_ignore: error_on_ignore) do |records|
+          records.each { |record| y << record }
+        end
+      }
+      if block_given?
+        enum.each { |row| yield row }
+      else
+        enum.each
+      end
+    end
+
+    def find_in_batches(start: nil, finish: nil, batch_size: 1000, error_on_ignore: nil)
+      enum = Enumerator.new { |y|
+        @relation.find_in_batches(start: start, finish: finish, batch_size: batch_size, error_on_ignore: error_on_ignore) do |records|
+          @preloads.each { |p| p.preload! records } if records.any?
+          y << records
+        end
+      }
+      if block_given?
+        enum.each { |records| yield records }
+      else
+        enum.each
       end
     end
   end
---
+++ test/query_test.rb
@@ -0,0 +1,75 @@
+require 'test_helper'
+
+class QueryTest < Minitest::Test
+  include TestHelpers
+
+  def setup
+    @queries = []
+    @sub = ActiveSupport::Notifications.subscribe 'sql.active_record' do |_name, _started, _finished, _uid, data|
+      @queries << [data.fetch(:sql), data.fetch(:type_casted_binds)]
+    end
+  end
+
+  def teardown
+    ActiveSupport::Notifications.unsubscribe @sub
+    @queries.clear
+  end
+
+  def test_preloading
+    categories = Uberloader
+      .query(Category.order(:name))
+      .preload(:widgets) { |u|
+        u.scope Widget.where.not(name: nil)
+        u.preload(:line_items)
+      }
+      .to_a
+
+    categories[0].widgets
+    assert_equal [
+      'SELECT "categories".* FROM "categories" ORDER BY "categories"."name" ASC',
+      'SELECT "widgets".* FROM "widgets" WHERE "widgets"."name" IS NOT NULL AND "widgets"."category_id" IN (?, ?)',
+      'SELECT "line_items".* FROM "line_items" WHERE "line_items"."item_type" = ? AND "line_items"."item_id" IN (?, ?, ?, ?, ?, ?, ?)',
+    ], @queries.map(&:first)
+
+    assert_equal Category.order(:name).pluck(:name), categories.map(&:name)
+    assert_equal Category.find(categories[0].id).widgets.pluck(:name), categories[0].widgets.map(&:name)
+  end
+
+  def test_find_in_batches
+    categories = []
+    Uberloader
+      .query(Category.order(:name))
+      .preload(:widgets) { |u|
+        u.scope Widget.where.not(name: nil)
+        u.preload(:line_items)
+      }
+      .find_in_batches { |records| categories.concat records }
+
+    assert_equal [
+      'SELECT "categories".* FROM "categories" ORDER BY "categories"."id" ASC LIMIT ?',
+      'SELECT "widgets".* FROM "widgets" WHERE "widgets"."name" IS NOT NULL AND "widgets"."category_id" IN (?, ?)',
+      'SELECT "line_items".* FROM "line_items" WHERE "line_items"."item_type" = ? AND "line_items"."item_id" IN (?, ?, ?, ?, ?, ?, ?)',
+    ], @queries.map(&:first)
+
+    assert_equal Category.order(:id).pluck(:name), categories.map(&:name)
+  end
+
+  def test_find_each
+    categories = []
+    Uberloader
+      .query(Category.order(:name))
+      .preload(:widgets) { |u|
+        u.scope Widget.where.not(name: nil)
+        u.preload(:line_items)
+      }
+      .find_each { |record| categories << record }
+
+    assert_equal [
+      'SELECT "categories".* FROM "categories" ORDER BY "categories"."id" ASC LIMIT ?',
+      'SELECT "widgets".* FROM "widgets" WHERE "widgets"."name" IS NOT NULL AND "widgets"."category_id" IN (?, ?)',
+      'SELECT "line_items".* FROM "line_items" WHERE "line_items"."item_type" = ? AND "line_items"."item_id" IN (?, ?, ?, ?, ?, ?, ?)',
+    ], @queries.map(&:first)
+
+    assert_equal Category.order(:id).pluck(:name), categories.map(&:name)
+  end
+end
